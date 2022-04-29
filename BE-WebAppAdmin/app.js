fe826bee96a503f18763e2664ab9ede8c2ef20b3
sergiogonzalez24 <sergiomanuel2020@gmail.com>
2022-04-28 22:59:31 -0500

    cambios otv

--- BE-WebAppAdmin/app.js
+++ BE-WebAppAdmin/app.js
@@ -4,11 +4,12 @@ const sequelize = new Sequelize('drumland.database.windows.net', 'sergiomaster',
   host: 'http://drumland.database.windows.net',
   dialect: 'mssql'
 });
+
 const app = express();
 
 sequelize.sync()
     .then(resultado=>{
         console.log("Conexion exitosa");
-        app.listen(8080,()=>console.log("Servidor en línea en el puerto 8080"));
+        app.listen();
     })
     .catch(error=>console.log(error))
