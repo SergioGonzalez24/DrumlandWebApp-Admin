823b88d3bea1683132072ec91e7aa67c1a81b708
sergiogonzalez24 <sergiomanuel2020@gmail.com>
2022-05-02 18:13:08 -0500

    aver

--- api/BE-DrumlandWebApp/index.js
+++ api/BE-DrumlandWebApp/index.js
@@ -1,6 +1,21 @@
 const sequelize = require("./util/database");
 const Admin = require("./models/adminPas");
+const path = require('path');
+const bodyParser = require('body-parser');
+const express = require('express');
+const adminRoutes = require("./routes/adminPas")
+const app = express();
 
+app.use(express.static(path.join(__dirname,'public')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+
+
+app.engine('html',require('ejs').renderFile);
+app.set('view engine','ejs');
+
+
+app.use('/admin',adminRoutes);
 
 
 sequelize.authenticate().then(()=>{
