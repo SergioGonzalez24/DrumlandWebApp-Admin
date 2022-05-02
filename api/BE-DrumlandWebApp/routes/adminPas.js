c86304121cd42e637821f36aee0fae14c306f1a9
sergiogonzalez24 <sergiomanuel2020@gmail.com>
2022-05-02 18:29:33 -0500

    borradio

--- api/BE-DrumlandWebApp/routes/adminPas.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const adminController = require('../controllers/adminPas');
-
-router.get('/',adminController.getUsuarios);
-
-
-module.exports = router;
