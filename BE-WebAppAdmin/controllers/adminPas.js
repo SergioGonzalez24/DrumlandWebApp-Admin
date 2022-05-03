7f8e893f3adff5fa9aed03046b2b9ed6fa85b5aa
sergiogonzalez24 <sergiomanuel2020@gmail.com>
2022-05-03 06:14:25 -0500

    jalara?

--- BE-WebAppAdmin/controllers/adminPas.js
+++ BE-WebAppAdmin/controllers/adminPas.js
@@ -1,6 +1,28 @@
 const adminPas = require('../models/adminPas');
 
-exports.login= (req,res, next)=>{
+exports.login = async(req,res )=>{
+    const username = req.body.username;
+    const password = req.body.password;
+
+    const user = await sequelize.query("SELECT * FROM adminPas WHERE Username = :username AND passwordUser = :password",
+    {replacements: {username: username, password: password}, type: sequelize.QueryTypes.SELECT
+    }
+    );
+
+    console.log(user);
     
+    if (user.valueOf() == 0){
+        res.json({
+            message: "Usuario o contraseña incorrecta"
+        })
+    }
+    else{
+        res.json({
+            message: "Usuario correcto"
+        })
+        res.redirect('/dashboard');
+    }
 }
+
+
 
--- BE-WebAppAdmin/routes/adminPas.js
+++ BE-WebAppAdmin/routes/adminPas.js
@@ -2,6 +2,6 @@ const express = require("express");
 const router = express.Router();
 const adminPas = require("../controllers/adminPas");
 
-router.get("/buscarUsuario", adminPas.getUsers);
+router.get("/login", adminPas.login);
 
 module.exports = router;
