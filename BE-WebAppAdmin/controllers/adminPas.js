9820ac6a25b222c78045b90483ff7f83e1be494d
sergiogonzalez24 <sergiomanuel2020@gmail.com>
2022-05-03 15:36:13 -0500

    cambios

--- BE-WebAppAdmin/controllers/adminPas.js
+++ BE-WebAppAdmin/controllers/adminPas.js
@@ -5,6 +5,15 @@ const adminPas = require('../models/adminPas');
 
 
 exports.getLogin = async(req,res )=>{
+    const password = req.body.password;
+    const username = req.body.username;
+
+    if (username === 'admin' && password === 'admin') {
+        res.redirect('/dashboard');
+    }
+    else {
+        res.message = 'Invalid username or password';
+    }
 
 
 }
